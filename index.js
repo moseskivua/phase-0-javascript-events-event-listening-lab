dabc59734d0fb04947bdb17b16a4f24e7b46941b
Moses Kivua <kivuamoses9@gmail.com>
2022-08-29 23:38:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+    alert(`${input.id} was clicked!`);
+    });
 }
+const input2 = document.getElementById('input');
+function clickAlert() {
+    //alert(`${input.id} was clicked!`);
+    alert('I was clicked!');
+}
+input2.addEventListener('click', clickAlert);
